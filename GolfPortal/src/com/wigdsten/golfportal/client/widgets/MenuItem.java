4b78574b481c37cc89ec46c52ccc55719461914f
Marcus <marcus.tinnsten@gmail.com>
2014-06-30 22:53:46 +0200

    Lagt till första utkastet på menyn!

--- GolfPortal/src/com/wigdsten/golfportal/client/widgets/MenuItem.java
+++ GolfPortal/src/com/wigdsten/golfportal/client/widgets/MenuItem.java
@@ -1,5 +1,30 @@
 package com.wigdsten.golfportal.client.widgets;
 
-public class MenuItem {
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+
+public class MenuItem extends HorizontalPanel implements HasClickHandlers {
+
+	public MenuItem(Image image, Label label) {
+		this.setStyleName("menuitem");
+		this.addStyleName("menuitem-selected");
+		label.addStyleName("menulabel");
+		image.addStyleName("menuitem-icon");
+		label.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+
+		this.add(image);
+		this.add(label);
+	}
+
+	@Override
+	public HandlerRegistration addClickHandler(ClickHandler handler) {
+		return addDomHandler(handler, ClickEvent.getType());
+	}
 
 }
--- GolfPortal/src/com/wigdsten/golfportal/client/widgets/MenuWidget.java
+++ GolfPortal/src/com/wigdsten/golfportal/client/widgets/MenuWidget.java
@@ -1,16 +1,38 @@
 package com.wigdsten.golfportal.client.widgets;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.wigdsten.golfportal.client.structure.MainPanel;
+import com.wigdsten.golfportal.client.ui.SandboxView;
 
-public class MenuWidget extends Composite {
+public class MenuWidget extends Composite implements ClickHandler {
 
-	FlowPanel flowPanel = new FlowPanel();
+	private final FlowPanel flowPanel = new FlowPanel();
+	private final SimplePanel mainPanel;
+	private final MenuItem itemSandbox;
 
 	public MenuWidget(MainPanel mainPanel) {
-		this.flowPanel.add(mainPanel);
+		this.mainPanel = mainPanel;
+		this.itemSandbox = new MenuItem(new Image(
+				"/images/Icons(16x16)/Toolbox.png"), new Label("Sandbox sidan"));
+		this.itemSandbox.addClickHandler(this);
+		this.flowPanel.add(this.itemSandbox);
 		initWidget(this.flowPanel);
+	}
+
+	@Override
+	public void onClick(ClickEvent event) {
+		Object source = event.getSource();
+		if (source.equals(this.itemSandbox)) {
+			SandboxView sandboxView = new SandboxView();
+			this.mainPanel.add(sandboxView);
+		}
+
 	}
 
 }
