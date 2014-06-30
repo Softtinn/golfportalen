9e78922f599a3140f65587d26d8638416777fbc9
Marcus <marcus.tinnsten@gmail.com>
2014-06-30 21:18:22 +0200

    Minor changes

--- GolfPortal/src/com/wigdsten/golfportal/server/DatabaseFacade.java
+++ GolfPortal/src/com/wigdsten/golfportal/server/DatabaseFacade.java
@@ -4,24 +4,56 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.classic.Session;
 
-import com.wigdsten.golfportal.client.TestInfoDTO;
 import com.wigdsten.golfportal.server.entities.TestInfo;
 
+@SuppressWarnings("deprecation")
 public class DatabaseFacade {
 
-	public void storeData(TestInfoDTO testInfoDTO) {
-		TestInfo contact = new TestInfo(testInfoDTO);
+	private SessionFactory sessionFactory;
+	private Session session;
 
-		SessionFactory sessionFactory = new AnnotationConfiguration()
-				.configure().buildSessionFactory();
+	public void openSession() {
+		if (this.sessionFactory == null) {
+			this.sessionFactory = new AnnotationConfiguration().configure()
+					.buildSessionFactory();
+		}
+		if (this.session == null) {
+			this.session = this.sessionFactory.openSession();
+		}
+	}
 
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		session.save(contact);
+	public void beginTransaction() {
+		if (this.session != null) {
+			this.session.beginTransaction();
+		}
+	}
 
-		session.getTransaction().commit();
-		session.close();
-		sessionFactory.close();
+	public void commitTransaction() {
+		if ((this.session != null) && this.session.getTransaction().isActive()) {
+			this.session.getTransaction().commit();
+		}
+	}
+
+	public void closeSession() {
+		if ((this.session != null) && this.session.isOpen()) {
+			this.session.close();
+			this.session = null;
+		}
+		if ((this.sessionFactory != null) && !this.sessionFactory.isClosed()) {
+			this.sessionFactory.close();
+			this.sessionFactory = null;
+		}
+	}
+
+	public void storeData(TestInfo testInfo) {
+
+		openSession();
+		beginTransaction();
+
+		this.session.save(testInfo);
+
+		commitTransaction();
+		closeSession();
 
 	}
 }
--- GolfPortal/src/com/wigdsten/golfportal/server/GolfPortalServiceImpl.java
+++ GolfPortal/src/com/wigdsten/golfportal/server/GolfPortalServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.wigdsten.golfportal.client.GolfPortalService;
 import com.wigdsten.golfportal.client.TestInfoDTO;
+import com.wigdsten.golfportal.server.entities.TestInfo;
 
 @SuppressWarnings("serial")
 public class GolfPortalServiceImpl extends RemoteServiceServlet implements
@@ -16,7 +17,8 @@ public class GolfPortalServiceImpl extends RemoteServiceServlet implements
 
 		// Can't set random UUID on client-side
 		testInfoDTO.setId(UUID.randomUUID().toString());
-		connection.storeData(testInfoDTO);
+		TestInfo testInfo = new TestInfo(testInfoDTO);
+		connection.storeData(testInfo);
 	}
 
 }
